916b188a0abd2b0f03e6ba0547295c09e2eebde8
Julie Clingo <julieclingo@gmail.com>
2015-09-02 16:14:41 -0600

    Variables and Functions

---
+++ Intro-to-Scripting/Assets/Behavior_1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Behavior_1 : MonoBehaviour {
+
+
+	int myInt = 55;
+
+	void Start (){
+		myInt = AddTwo(myInt);
+		Debug.Log (myInt);
+	}
+	
+	int AddTwo (int number)
+	{
+		int ret;
+		ret = number + 2;
+		return ret;
+	}
+}
